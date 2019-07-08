2790800ba7709dd2918cc44418f97909135ed134
Reshmi <Reshmi_R_S@external.mckinsey.com>
2019-07-08 19:26:18 +0530

    Learning axios

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import axios from "axios";
+import emojis from "./private/emoji";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.setEmoji = this.setEmoji.bind(this);
+    this.modifyEmojiState = this.modifyEmojiState.bind(this);
+    this.render = this.render.bind(this);
+    this.Emojis = this.Emojis.bind(this);
+    this.generateOption = this.generateOption.bind(this);
+    this.state = { moji: "", emojis: emojis };
+  }
+
+  modifyEmojiState(value) {
+    this.setState({ moji: value });
+  }
+
+  setEmoji() {
+    const emoji = document.getElementById("emoji").value; 
+    axios
+      .get(`https://www.emojidex.com/api/v1/emoji/${emoji}`)
+      .then(res => this.modifyEmojiState(res.data.moji));
+  }
+
+  generateOption(key) {
+    return <option value={this.state.emojis[key]}>{key}</option>;
+  }
+
+  Emojis() {
+    const emojis = Object.keys(this.state.emojis).map(key =>
+      this.generateOption(key)
+    );
+    return emojis;
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="search">
+          <select id="emoji">
+            <this.Emojis />
+          </select>
+          <button onClick={this.setEmoji}>Submit</button>
+        </div>
+        <div className="emoji">{this.state.moji}</div>
+      </div>
+    );
+  }
 }
 
 export default App;
